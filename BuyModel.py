db26e12e5624d9c0c09434f73a4ac3f30b7c0be0
Matthew Voss <matthewpvoss@gmail.com>
2016-06-29 21:26:18 -0500

    Organized file structure

--- BuyModel.py
+++
@@ -1,7 +0,0 @@
-import numpy
-def shouldBuy(ticker, data):
-  mean = numpy.mean(data['Close'])
-  last = data['Close'][-1]
-
-  print(ticker + ": Mean: " + str(mean) + ", current: " + str(last)) 
-  return last < mean      
